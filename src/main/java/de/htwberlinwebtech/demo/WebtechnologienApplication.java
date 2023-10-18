d98ded38452a28a4f8a7523cb90063d7dff7cd20
MeteHan98 <s0584169@htw-berlin.de>
2023-10-18 12:23:47 +0200

    Hello World

---
+++ src/main/java/de/htwberlinwebtech/demo/WebtechnologienApplication.java
@@ -0,0 +1,13 @@
+package de.htwberlinwebtech.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtechnologienApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtechnologienApplication.class, args);
+	}
+
+}
